8c53c7aa0e253c71b05b1c8ab3439e646fd06746
Yu Koga <koga@tagbangers.co.jp>
2017-09-06 18:19:42 +0900

    copypaste

--- app/src/main/java/com/example/koga/sample/MainActivity.java
+++ app/src/main/java/com/example/koga/sample/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +16,46 @@ import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private RecyclerView mRecyclerView1;
+    private RecyclerView.Adapter mAdapter1;
+    private RecyclerView.LayoutManager mLayoutManager1;
+
+    private String[] myDataset1 = new String[20];
+
+    private RecyclerView mRecyclerView2;
+    private RecyclerView.Adapter mAdapter2;
+    private RecyclerView.LayoutManager mLayoutManager2;
+
+    private String[] myDataset2 = new String[20];
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView1 = (RecyclerView)(findViewById(R.id.recycler_view_left));
+        mRecyclerView1.setHasFixedSize(true);
+        mLayoutManager1 = new LinearLayoutManager(this);
+        mRecyclerView1.setLayoutManager(mLayoutManager1);
+        for(int i=0; i<myDataset1.length; i++) {
+            myDataset1[i] = "Data_0"+String.valueOf(i);
+        }
+        mAdapter1 = new MyAdapter1(myDataset1);
+        mRecyclerView1.setAdapter(mAdapter1);
+
+
+        mRecyclerView2 = (RecyclerView)(findViewById(R.id.recycler_view_right));
+        mRecyclerView2.setHasFixedSize(true);
+        mLayoutManager2 = new LinearLayoutManager(this);
+        mRecyclerView2.setLayoutManager(mLayoutManager2);
+        for(int i=0; i<myDataset2.length; i++) {
+            myDataset2[i] = "Data_0"+String.valueOf(i);
+        }
+        mAdapter2 = new MyAdapter2(myDataset2);
+        mRecyclerView2.setAdapter(mAdapter2);
+
+
+
 
         View bottomSheet = findViewById(R.id.bottomSheet1);
         final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
---
+++ app/src/main/java/com/example/koga/sample/MyAdapter2.java
@@ -0,0 +1,56 @@
+package com.example.koga.sample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by koga on 2017/09/06.
+ */
+
+public class MyAdapter2 extends RecyclerView.Adapter<MyAdapter2.ViewHolder> {
+
+    private String[] mDataset = new String[20];
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+
+        public ViewHolder(View v){
+            super(v);
+            mTextView = (TextView)(v.findViewById(R.id.list_item_text));
+        }
+    }
+
+    public MyAdapter2(String[] myDataset2){
+        mDataset = myDataset2;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+
+        // set the view's size, margins, paddings and layout parameters
+
+
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
