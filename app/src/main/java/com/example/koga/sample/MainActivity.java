373f631d7fb584b80a71799e78b1f535d610eea4
Yu Koga <koga@tagbangers.co.jp>
2017-09-19 11:29:24 +0900

    switch areas by clicking different floors

--- app/src/main/java/com/example/koga/sample/MainActivity.java
+++ app/src/main/java/com/example/koga/sample/MainActivity.java
@@ -12,24 +12,30 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> floorList;
-    ArrayList<String> areaList;
 
     ListView listView;
     ListView listView2;
 
-    ArrayAdapter<String> adapter;
-    ArrayAdapter<String> adapter2;
-    ArrayAdapter<String> adapter3;
+    ArrayAdapter<String> floorAdapter;
+    ArrayAdapter<String> areaAdapter;
+    ArrayAdapter<String> areaAdapter2;
+    ArrayAdapter<String> areaAdapter3;
+    ArrayAdapter<String> areaAdapter4;
+    ArrayAdapter<String> areaAdapter5;
+    ArrayAdapter<String> areaAdapter6;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
 
         // Create a floor list
         floorList = new ArrayList<>();
@@ -43,39 +49,59 @@ public class MainActivity extends AppCompatActivity {
         floorList.add("6F");
 
         // Create an adapter
-        adapter = new ArrayAdapter<>(this, R.layout.list_item, floorList);
+        floorAdapter = new ArrayAdapter<>(this, R.layout.list_item, floorList);
 
         // Link Adapter to ListView
         listView = (ListView) findViewById(R.id.listView1);
-        listView.setAdapter(adapter);
+        listView.setAdapter(floorAdapter);
 
         // Create an area list
-        areaList = new ArrayList<>();
-
+        ArrayList<String> firstFloor = new ArrayList<>();
         // Put data in the list
-        areaList.add("エリアA");
-        areaList.add("エリアB");
-        areaList.add("エリアC");
-        areaList.add("エリアD");
-        areaList.add("エリアE");
-        areaList.add("エリアF");
+        firstFloor.add("エリアA");
 
-        // Create an area list
-        ArrayList<String> areaList2 = new ArrayList<>();
+        ArrayList<String> secondFloor = new ArrayList<>();
+        secondFloor.add("エリアA");
+        secondFloor.add("エリアB");
 
-        // Put data in the list
-        areaList2.add("エリアA");
-        areaList2.add("エリアB");
-        areaList2.add("エリアC");
+        ArrayList<String> thirdFloor = new ArrayList<>();
+        thirdFloor.add("エリアA");
+        thirdFloor.add("エリアB");
+        thirdFloor.add("エリアC");
+
+        ArrayList<String> forthFloor = new ArrayList<>();
+        forthFloor.add("エリアA");
+        forthFloor.add("エリアB");
+        forthFloor.add("エリアC");
+        forthFloor.add("エリアD");
+
+        ArrayList<String> fifthFloor = new ArrayList<>();
+        fifthFloor.add("エリアA");
+        fifthFloor.add("エリアB");
+        fifthFloor.add("エリアC");
+        fifthFloor.add("エリアD");
+        fifthFloor.add("エリアE");
+
+        ArrayList<String> sixthFloor = new ArrayList<>();
+        sixthFloor.add("エリアA");
+        sixthFloor.add("エリアB");
+        sixthFloor.add("エリアC");
+        sixthFloor.add("エリアD");
+        sixthFloor.add("エリアE");
+        sixthFloor.add("エリアF");
 
 
         // Create an adapter
-        adapter2 = new ArrayAdapter<>(this, R.layout.list_item, areaList);
-        adapter3 = new ArrayAdapter<>(this, R.layout.list_item, areaList2);
+        areaAdapter = new ArrayAdapter<>(this, R.layout.list_item, firstFloor);
+        areaAdapter2 = new ArrayAdapter<>(this, R.layout.list_item, secondFloor);
+        areaAdapter3 = new ArrayAdapter<>(this, R.layout.list_item, thirdFloor);
+        areaAdapter4 = new ArrayAdapter<>(this, R.layout.list_item, forthFloor);
+        areaAdapter5 = new ArrayAdapter<>(this, R.layout.list_item, fifthFloor);
+        areaAdapter6 = new ArrayAdapter<>(this, R.layout.list_item, sixthFloor);
 
         // Link Adapter to ListView
         listView2 = (ListView) findViewById(R.id.listView2);
-        listView2.setAdapter(adapter2);
+        listView2.setAdapter(areaAdapter);
 
 //        onListItemClick(listView.getPositionForView(listView));
 
@@ -131,12 +157,29 @@ public class MainActivity extends AppCompatActivity {
 
     @OnItemClick(R.id.listView1)
     public void onListItemClick(int position) {
-//        if (position != 1){
-//            areaList.add("エリアX");
-//            areaList.add("エリアY");
-//            areaList.add("エリアZ");
-//            adapter2.notifyDataSetChanged();
+//        if (position == 1){
+//            listView2.setAdapter(areaAdapter2);
 //        }
+        switch (position) {
+            case 0:
+                listView2.setAdapter(areaAdapter);
+                break;
+            case 1:
+                listView2.setAdapter(areaAdapter2);
+                break;
+            case 2:
+                listView2.setAdapter(areaAdapter3);
+                break;
+            case 3:
+                listView2.setAdapter(areaAdapter4);
+                break;
+            case 4:
+                listView2.setAdapter(areaAdapter5);
+                break;
+            case 5:
+                listView2.setAdapter(areaAdapter6);
+                break;
+        }
         Log.v("ListView", "selected pos=" + position);
     }
 }
